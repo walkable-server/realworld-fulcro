707089d776ab10554ef4db2d956bbe622e1e202a
Hoàng Minh Thắng <p@banphim.net>
2018-04-24 16:06:45 +0700

    add profiles

--- src/conduit/handler/user.clj
+++ src/conduit/handler/user.clj
@@ -11,6 +11,13 @@
     (let [id (user/create-user db email username password)]
       [::response/created (str "/users/" id)])))
 
+(defmethod ig/init-key ::destroy [_ {:keys [db]}]
+  (fn [{[_ user-id] :ataraxy/result}]
+    [::response/ok "cool"]
+    #_
+    (let [id (user/destroy-user db email username password)]
+      [::response/created (str "/users/" id)])))
+
 (defmethod ig/init-key ::login [_ {:keys [db secret]}]
   (fn [{[_kw username password] :ataraxy/result}]
     (if-let [user (user/find-user db username password)]
@@ -21,6 +28,17 @@
   [_ opts]
   (fn [{id :identity}]
     [::response/ok {:message (str "your user id: " (:user-id id))}]))
+
+(defmethod ig/init-key ::by-username
+  [_ {:keys [resolver]}]
+  (fn [{[_kw username] :ataraxy/result
+        id             :identity}]
+    [::response/ok
+     (resolver (:user-id id)
+       `[{(:users/all {:filters [:= :user/username ~username]})
+          [:user/id :user/email :user/username :user/bio :user/image
+           {:user/followed-by-me? [:agg/count]}
+           {:user/followed-by [:user/id :user/username]}]}])]))
 
 (defmethod ig/init-key ::all-users
   [_ {:keys [resolver]}]
