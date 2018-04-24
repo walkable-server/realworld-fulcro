969b3dd6fc15179230f27c40ea3ab439ebe33a8c
Hoàng Minh Thắng <p@banphim.net>
2018-04-24 09:23:41 +0700

    update project deps

--- project.clj
+++ project.clj
@@ -6,8 +6,12 @@
                  [duct/core "0.6.2"]
                  [duct/module.logging "0.3.1"]
                  [duct/module.web "0.6.4"]
+                 [duct/middleware.buddy "0.1.0"]
                  [duct/module.ataraxy "0.2.0"]
                  [duct/module.sql "0.4.2"]
+                 [duct/handler.sql "0.3.1"]
+                 [buddy/buddy-hashers "1.3.0"]
+                 [walkable "1.0.0-SNAPSHOT"]
                  [org.postgresql/postgresql "42.1.4"]]
   :plugins [[duct/lein-duct "0.10.6"]]
   :main ^:skip-aot conduit.main
