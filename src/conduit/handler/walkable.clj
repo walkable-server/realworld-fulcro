0be69ae0dd41850fd92bed2f1c3812c762457239
Hoàng Minh Thắng <p@banphim.net>
2018-04-24 13:03:59 +0700

    add derive-count-to-boolean plugin

--- src/conduit/handler/walkable.clj
+++ src/conduit/handler/walkable.clj
@@ -3,12 +3,23 @@
             [integrant.core :as ig]
             [com.wsscode.pathom.core :as p]))
 
+(def derive-count-to-boolean
+  {::p/wrap-read
+   (fn [reader]
+     (fn [env]
+       (let [k (-> env :ast :dispatch-key)]
+         (if (#{:article/liked-by-me? :user/followed-by-me?} k)
+           (let [{n :agg/count} (reader env)]
+             (not= 0 n))
+           (reader env)))))})
+
 (def pathom-parser
   (p/parser
     {::p/plugins
      [(p/env-plugin
         {::p/reader
-         [sqb/pull-entities p/map-reader]})]}))
+         [sqb/pull-entities p/map-reader]})
+      derive-count-to-boolean]}))
 
 (def extra-conditions
   {:articles/feed
