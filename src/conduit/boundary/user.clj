b3dd49e908370b0bf9f18a21678fade4861ec8d8
Hoàng Minh Thắng <p@banphim.net>
2018-04-26 18:22:36 +0700

    add user/by-id

--- src/conduit/boundary/user.clj
+++ src/conduit/boundary/user.clj
@@ -4,6 +4,7 @@
             [duct.database.sql]))
 
 (defprotocol User
+  (by-id [db id])
   (create-user [db user])
   (find-login [db email password])
   (follow [db follower-id followee-id])
@@ -17,6 +18,9 @@
                     (-> user (select-keys [:username :email :bio :image])
                       (assoc :password pw-hash)))]
       (-> results first (dissoc :password))))
+  (by-id [{db :spec} id]
+    (when-let [user (first (jdbc/find-by-keys db "\"user\"" {:id id}))]
+      (dissoc user :password)))
   (find-login [{db :spec} email password]
     (when-let [user (first (jdbc/find-by-keys db "\"user\"" {:email email}))]
       (when (hashers/check password (:password user))
