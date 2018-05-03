1dbfba114f9a23297504b9fb8b038e9185478f5a
Hoàng Minh Thắng <p@banphim.net>
2018-05-03 19:31:30 +0700

    add mutations impl

---
+++ src/conduit/api/mutations.clj
@@ -0,0 +1,7 @@
+(ns conduit.api.mutations
+  (:require
+    [taoensso.timbre :as timbre]
+    [fulcro.server :refer [defmutation]]))
+
+;; Place your server mutations here
+
---
+++ src/conduit/api/mutations.cljs
@@ -0,0 +1,7 @@
+(ns conduit.api.mutations
+  (:require
+    [fulcro.client.mutations :refer [defmutation]]
+    [fulcro.client.logging :as log]))
+
+;; Place your client mutations here
+
