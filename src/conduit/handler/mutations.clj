15757e279431d493edc997c55eccb516d0ffada6
Hoàng Minh Thắng <p@banphim.net>
2018-05-05 18:32:46 +0700

    implement mutations/submit-article

--- src/conduit/handler/mutations.clj
+++ src/conduit/handler/mutations.clj
@@ -1,5 +1,7 @@
 (ns conduit.handler.mutations
   (:require [conduit.boundary.user :as user]
+            [clojure.set :refer [rename-keys]]
+            [conduit.boundary.article :as article]
             [buddy.sign.jwt :as jwt]
             [buddy.hashers :as hashers]
             [walkable.sql-query-builder :as sqb]
@@ -13,4 +15,13 @@
   (action [{:keys [duct/logger app/jwt-secret] ::sqb/keys [sql-db] :as env}]
     (if-let [user (user/find-login sql-db email password)]
       (token user jwt-secret)
+      {})))
+
+(defmutation submit-article [{id :article/id article :diff}]
+  (action [{:keys [duct/logger] ::sqb/keys [sql-db] :app/keys [current-user]}]
+    ;;(log logger :info :article article)
+    (if current-user
+      (article/update-article sql-db current-user id
+        (-> (get article [:article/by-id id])
+          (rename-keys {:article/title :title})))
       {})))
