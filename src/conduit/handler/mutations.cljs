c72c504edb59e7bdc71a4e72e5aaf54ea275d2cf
Hoàng Minh Thắng <p@banphim.net>
2018-05-05 18:33:18 +0700

    implement mutations/submit-article for client

--- src/conduit/handler/mutations.cljs
+++ src/conduit/handler/mutations.cljs
@@ -1,7 +1,13 @@
 (ns conduit.handler.mutations
-  (:require [fulcro.client.mutations :refer [defmutation]]))
+  (:require [fulcro.client.mutations :refer [defmutation]]
+            [fulcro.ui.form-state :as fs]))
 
 (defmutation login [{:keys [email password]}]
   (action [{:keys [state]}] state)
   (refresh [env] [:user/whoami])
+  (remote [env] true))
+
+(defmutation submit-article [{:article/keys [id]}]
+  (action [{:keys [state]}]
+    (swap! state fs/entity->pristine* [:article/by-id id]))
   (remote [env] true))
