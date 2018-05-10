23386ad3fd11fe8ece6e4be8748a4abe1cb5a107
Hoàng Minh Thắng <p@banphim.net>
2018-05-10 21:13:42 +0700

    remove login mutation

--- src/conduit/handler/mutations.clj
+++ src/conduit/handler/mutations.clj
@@ -2,20 +2,9 @@
   (:require [conduit.boundary.user :as user]
             [clojure.set :refer [rename-keys]]
             [conduit.boundary.article :as article]
-            [buddy.sign.jwt :as jwt]
-            [buddy.hashers :as hashers]
             [walkable.sql-query-builder :as sqb]
             [duct.logger :refer [log]]
             [fulcro.server :as server :refer [defmutation]]))
-
-(defn token [user jwt-secret]
-  {:token (jwt/sign {:user/id (:id user)} jwt-secret)})
-
-(defmutation login [{:keys [email password]}]
-  (action [{:keys [duct/logger app/jwt-secret] ::sqb/keys [sql-db] :as env}]
-    (if-let [user (user/find-login sql-db email password)]
-      (token user jwt-secret)
-      {})))
 
 (defmutation submit-article [{id :article/id article :diff}]
   (action [{:keys [duct/logger] ::sqb/keys [sql-db] :app/keys [current-user]}]
--- src/conduit/handler/mutations.cljs
+++ src/conduit/handler/mutations.cljs
@@ -2,11 +2,6 @@
   (:require [fulcro.client.mutations :refer [defmutation]]
             [fulcro.ui.form-state :as fs]))
 
-(defmutation login [{:keys [email password]}]
-  (action [{:keys [state]}] state)
-  (refresh [env] [:user/whoami])
-  (remote [env] true))
-
 (defmutation submit-article [{:article/keys [id]}]
   (action [{:keys [state]}]
     (swap! state fs/entity->pristine* [:article/by-id id]))
