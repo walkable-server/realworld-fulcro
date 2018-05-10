88c98033fcf32b7ec4a2954066cfae2227d8f558
Hoàng Minh Thắng <p@banphim.net>
2018-05-10 23:54:49 +0700

    refactor mutations.clj

--- src/conduit/handler/mutations.clj
+++ src/conduit/handler/mutations.clj
@@ -3,15 +3,30 @@
             [clojure.set :refer [rename-keys]]
             [conduit.boundary.article :as article]
             [walkable.sql-query-builder :as sqb]
+            [conduit.util :as util]
             [duct.logger :refer [log]]
             [fulcro.server :as server :refer [defmutation]]))
 
-(defmutation submit-article [{id :article/id article :diff}]
+(def remove-user-namespace
+  (util/remove-namespace "user" [:username :name :email :bio :image :password]))
+
+(def remove-article-namespace
+  (util/remove-namespace "article" [:title :body :slug :description]))
+
+(defmutation submit-article [diff]
   (action [{:keys [duct/logger] ::sqb/keys [sql-db] :app/keys [current-user]}]
-    ;;(log logger :info :article article)
+    ;;(log logger :info :article diff)
     (if current-user
-      (article/update-article sql-db current-user id
-        (-> (get article [:article/by-id id])
-          (rename-keys {:article/title :title :article/body :body
-                        :article/slug :slug :article/description :description})))
+      (article/update-article sql-db current-user
+        (second (util/get-ident diff)) ;; article-id
+        (-> (util/get-item diff)
+          (rename-keys remove-article-namespace)))
+      {})))
+
+(defmutation submit-settings [diff]
+  (action [{:keys [duct/logger] ::sqb/keys [sql-db] :app/keys [current-user]}]
+    ;;(log logger :info :settings diff)
+    (if current-user
+      (user/update-user sql-db current-user
+        (-> (util/get-item diff) (rename-keys remove-user-namespace)))
       {})))
