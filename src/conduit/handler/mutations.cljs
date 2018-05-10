70e2f4a259b38bc5e28d113548e711b324bda7e7
Hoàng Minh Thắng <p@banphim.net>
2018-05-11 00:43:22 +0700

    implement client side submit-settings

--- src/conduit/handler/mutations.cljs
+++ src/conduit/handler/mutations.cljs
@@ -1,8 +1,14 @@
 (ns conduit.handler.mutations
   (:require [fulcro.client.mutations :refer [defmutation]]
+            [conduit.util :as util]
             [fulcro.ui.form-state :as fs]))
 
 (defmutation submit-article [{:article/keys [id]}]
   (action [{:keys [state]}]
     (swap! state fs/entity->pristine* [:article/by-id id]))
+  (remote [env] true))
+
+(defmutation submit-settings [diff]
+  (action [{:keys [state]}]
+    (swap! state fs/entity->pristine* (util/get-ident diff)))
   (remote [env] true))
