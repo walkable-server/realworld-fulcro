88d3c43a637a2fb8fe0f35a1f6e2c9ee580e3e2a
Hoàng Minh Thắng <p@banphim.net>
2018-05-11 16:03:36 +0700

    support #fulcro/tempid in transit+json

---
+++ src/conduit/handler/transit_format.clj
@@ -0,0 +1,16 @@
+(ns conduit.handler.transit-format
+  (:require [muuntaja.format.transit :as mt]
+            [integrant.core :as ig])
+  (:import  [fulcro.tempid TempId]
+            [com.cognitect.transit ReadHandler]
+            [fulcro.transit TempIdHandler]))
+
+(defmethod ig/init-key ::transit-json-format [_ _params]
+  {:decoder [(fn [opts] (mt/make-transit-decoder :json
+                          (assoc-in opts
+                            [:handlers "fulcro/tempid"]
+                            (reify
+                              ReadHandler
+                              (fromRep [_ id] (TempId. id))))))]
+   :encoder [(fn [opts] (mt/make-transit-encoder :json
+                          (assoc-in opts [:handlers TempId] (TempIdHandler.))))]})
