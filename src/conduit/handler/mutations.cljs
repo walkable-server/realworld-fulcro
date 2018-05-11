d33aae22a96ee7b249e7b93c1146087074737adc
Hoàng Minh Thắng <p@banphim.net>
2018-05-11 17:06:36 +0700

    fix wrong arg destructuring

--- src/conduit/handler/mutations.cljs
+++ src/conduit/handler/mutations.cljs
@@ -3,9 +3,9 @@
             [conduit.util :as util]
             [fulcro.ui.form-state :as fs]))
 
-(defmutation submit-article [{:article/keys [id]}]
+(defmutation submit-article [diff]
   (action [{:keys [state]}]
-    (swap! state fs/entity->pristine* [:article/by-id id]))
+    (swap! state fs/entity->pristine* (util/get-ident diff)))
   (remote [env] true))
 
 (defmutation submit-settings [diff]
