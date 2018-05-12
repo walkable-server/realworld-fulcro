15da910afe772fa7ac02a621bdce7691b315a24a
Hoàng Minh Thắng <p@banphim.net>
2018-05-12 21:19:28 +0700

    update mutations to use app/db

--- src/conduit/handler/mutations.clj
+++ src/conduit/handler/mutations.clj
@@ -16,22 +16,22 @@
   (util/remove-namespace "article" [:title :body :slug :description]))
 
 (defmutation submit-article [diff]
-  (action [{:keys [duct/logger] ::sqb/keys [sql-db] :app/keys [current-user]}]
+  (action [{:keys [duct/logger] :app/keys [db current-user]}]
     ;;(log logger :info :article diff)
     (if current-user
       (let [[_ article-id] (util/get-ident diff)
             article        (-> (util/get-item diff)
                              (rename-keys remove-article-namespace))]
         (if (tempid? article-id)
-          (let [new-id (article/create-article sql-db current-user article)]
+          (let [new-id (article/create-article db current-user article)]
             {::prim/tempids {article-id new-id}})
-          (article/update-article sql-db current-user article-id article)))
+          (article/update-article db current-user article-id article)))
       {})))
 
 (defmutation submit-settings [diff]
-  (action [{:keys [duct/logger] ::sqb/keys [sql-db] :app/keys [current-user]}]
+  (action [{:keys [duct/logger] :app/keys [db current-user]}]
     ;;(log logger :info :settings diff)
     (if current-user
-      (user/update-user sql-db current-user
+      (user/update-user db current-user
         (-> (util/get-item diff) (rename-keys remove-user-namespace)))
       {})))
