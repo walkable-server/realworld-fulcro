7de9e710a77530a8a28ceb07cb0fb7e957230f58
Hoàng Minh Thắng <p@banphim.net>
2018-05-17 10:50:08 +0700

    remove unused files

--- src/conduit/api/mutations.clj
+++
@@ -1,7 +0,0 @@
-(ns conduit.api.mutations
-  (:require
-    [taoensso.timbre :as timbre]
-    [fulcro.server :refer [defmutation]]))
-
-;; Place your server mutations here
-
--- src/conduit/api/mutations.cljs
+++
@@ -1,7 +0,0 @@
-(ns conduit.api.mutations
-  (:require
-    [fulcro.client.mutations :refer [defmutation]]
-    [fulcro.client.logging :as log]))
-
-;; Place your client mutations here
-
