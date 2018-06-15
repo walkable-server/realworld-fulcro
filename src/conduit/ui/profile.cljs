a1636679b60dc9b0a48febd3f7d79717c71f915e
Hoàng Minh Thắng <p@banphim.net>
2018-06-15 13:47:08 +0700

    update Profile with new pagination

--- src/conduit/ui/profile.cljs
+++ src/conduit/ui/profile.cljs
@@ -2,65 +2,21 @@
   (:require
    [fulcro.client.primitives :as prim :refer [defsc]]
    [conduit.handler.mutations :as mutations]
-   [conduit.ui.other :as other]
    [conduit.ui.comment :as comment]
    [conduit.ui.routes :as routes]
    [fulcro.client.mutations :as m :refer [defmutation]]
    [fulcro.client.data-fetch :as df]
    [fulcro.client.routing :as r]
    [fulcro.client.dom :as dom]
-   [conduit.ui.article-preview :as preview]))
-
-(defsc LikedArticles [this {:user/keys [id] articles :user/like}]
-  {:ident         [:user/by-id :user/id]
-   :initial-state (fn [params] #:user{:id :guest :like []})
-   :query         [:user/id {:user/like (prim/get-query preview/ArticlePreview)}]}
-  (preview/article-list this articles "This user liked no article!"))
-
-(def ui-liked-articles (prim/factory LikedArticles))
-
-(defsc LikedArticlesScreen
-  [this {:keys [screen profile-to-view user-id]}]
-  {:initial-state (fn [params]
-                    {:screen          :screen.liked-articles/by-user-id
-                     :user-id         :guest
-                     :profile-to-view (prim/get-initial-state LikedArticles #:user{:id :guest})})
-   :ident         (fn [] [screen user-id])
-   :query         [:screen :user-id {:profile-to-view (prim/get-query LikedArticles)}]}
-  (ui-liked-articles profile-to-view))
-
-(defsc OwnedArticles [this {:user/keys [id articles]}]
-  {:ident         [:user/by-id :user/id]
-   :initial-state (fn [params] #:user{:id :guest :articles []})
-   :query         [:user/id {:user/articles (prim/get-query preview/ArticlePreview)}]}
-  (preview/article-list this articles "This user has no article!"))
-
-(def ui-owned-articles (prim/factory OwnedArticles))
-
-(defsc OwnedArticlesScreen
-  [this {:keys [screen profile-to-view user-id]}]
-  {:initial-state (fn [params]
-                    {:screen          :screen.owned-articles/by-user-id
-                     :user-id         :guest
-                     :profile-to-view (prim/get-initial-state OwnedArticles #:user {:id :guest})})
-   :ident         (fn [] [screen user-id])
-   :query         [:screen :user-id {:profile-to-view (prim/get-query OwnedArticles)}]}
-  (ui-owned-articles profile-to-view))
-
-(r/defrouter ProfileRouter :router/profile
-  [:screen :user-id]
-  :screen.owned-articles/by-user-id OwnedArticlesScreen
-  :screen.liked-articles/by-user-id LikedArticlesScreen)
-
-(def ui-profile-router (prim/factory ProfileRouter))
+   [conduit.ui.article-preview :as preview]
+   [conduit.ui.pagination :as pagination]
+   [conduit.util :as util]))
 
 (defsc Profile [this {:user/keys [id name username image bio followed-by-me followed-by-count]}]
   {:ident         [:user/by-id :user/id]
-   :initial-state (fn [params] #:user {:id :guest :like [] :articles []})
+   :initial-state (fn [params] #:user {:id :guest})
    :query         [:user/id :user/name :user/username :user/image :user/bio
-                   :user/followed-by-me :user/followed-by-count
-                   {:user/like (prim/get-query preview/ArticlePreview)}
-                   {:user/articles (prim/get-query preview/ArticlePreview)}]}
+                   :user/followed-by-me :user/followed-by-count]}
   (dom/div :.user-info
     (dom/div :.container
       (dom/div :.row
@@ -84,81 +40,76 @@
 
 (def ui-profile (prim/factory Profile))
 
-(defsc ListSelector [this props {:user/keys [id name]}]
-  {:query [[r/routers-table '_]]}
-  (let [[current-screen _] (r/current-route props :router/profile)
-        whoami             (prim/shared this :user/whoami)]
+(defsc ListSelector [this props {:keys [current-page current-user-name]}]
+  {:query []}
+  (let [{:pagination/keys [list-type list-id]} current-page
+        whoami                                 (prim/shared this :user/whoami)]
     (dom/div :.articles-toggle
       (dom/ul :.nav.nav-pills.outline-active
         (dom/li :.nav-item
           (dom/div :.nav-link
-            {:className (when (= current-screen :screen.owned-articles/by-user-id) "active")
-             :onClick   #(routes/go-to-profile this {:user/id id})}
-            (if (= (:user/id whoami) id)
+            {:className (when (= list-type :owned-articles/by-user-id) "active")
+             :onClick   #(prim/transact! this `[(load-page #:pagination{:list-type :owned-articles/by-user-id
+                                                                        :list-id   ~list-id
+                                                                        :size      5})])}
+            (if (= (:user/id whoami) list-id)
               "My"
-              (str name "'s"))
+              (str current-user-name "'s"))
             " Articles"))
         (dom/li :.nav-item
           (dom/div :.nav-link
-            {:className (when (= current-screen :screen.liked-articles/by-user-id) "active")
-             :onClick #(routes/go-to-liked-article this {:user/id id})}
+            {:className (when (= list-type :liked-articles/by-user-id) "active")
+             :onClick   #(prim/transact! this `[(load-page #:pagination {:list-type :liked-articles/by-user-id
+                                                                         :list-id   ~list-id
+                                                                         :size      5})])}
             "Favorited Articles"))))))
 
 (def ui-list-selector (prim/factory ListSelector))
 
-(defsc ProfileScreen [this {:keys  [screen profile-to-view user-id list-selector]
-                            router [r/routers-table :router/profile]}]
+(defsc ProfileScreen [this {:keys  [screen profile-to-view user-id current-page]}]
   {:ident         (fn [] [screen user-id])
-   :initial-state (fn [params] {:screen          :screen.profile/by-user-id
-                                :user-id         :guest
-                                :list-selector   {}
-                                :profile-to-view (prim/get-initial-state Profile #:user{:id :guest})})
+   :initial-state (fn [params] {:screen                :screen.profile/by-user-id
+                                :user-id               :guest
+                                :profile-to-view       (prim/get-initial-state Profile #:user{:id :guest})})
    :query         (fn [] [:screen :user-id
-                          {[r/routers-table :router/profile] (prim/get-query ProfileRouter)}
-                          {:list-selector (prim/get-query ListSelector)}
+                          {:current-page (prim/get-query pagination/Page)}
                           {:profile-to-view (prim/get-query Profile)}])}
   (dom/div :.profile-page
     (ui-profile profile-to-view)
     (dom/div :.container
       (dom/div :.row
         (dom/div :.col-xs-12.col-md-10.offset-md-1
-          (ui-list-selector (prim/computed list-selector profile-to-view))
-          (ui-profile-router router))))))
+          (ui-list-selector (prim/computed {} {:current-page current-page
+                                               :current-user-name (:user/name profile-to-view)}))
+          (pagination/ui-page (prim/computed current-page {:load-page #(prim/transact! this `[(load-page ~%)])})))))))
 
-(defmutation load-profile-to-screen [{:user/keys [id]}]
+(defmutation load-profile [{:user/keys [id]}]
   (action [{:keys [state] :as env}]
-    (df/load-action env [:user/by-id id] Profile {:without #{:user/like}})
     (swap! state
-      #(-> %
-         (assoc-in [:screen.profile/by-user-id id]
-           {:screen          :screen.profile/by-user-id
-            :user-id         id
-            :list-selector   {}
-            :profile-to-view [:user/by-id id]})
-         (assoc-in [:screen.owned-articles/by-user-id id]
-           {:screen          :screen.owned-articles/by-user-id
-            :user-id         id
-            :profile-to-view [:user/by-id id]}))))
+      #(update-in % [:screen.profile/by-user-id id]
+         (fn [x] (if x
+                   x
+                   {:screen          :screen.profile/by-user-id
+                    :user-id         id
+                    :current-page    {}
+                    :profile-to-view [:user/by-id id]}))))
+    (df/load-action env [:user/by-id id] Profile)
+    (df/load-action env :paginated-list/articles
+      pagination/Page {:params #:pagination{:list-type :owned-articles/by-user-id
+                                            :list-id id
+                                            :size 5}
+                       :target [:screen.profile/by-user-id id :current-page]}))
   (remote [env]
     (df/remote-load env))
   (refresh [env] [:screen :profile-to-view]))
 
-(defmutation load-liked-articles-to-screen [{:user/keys [id]}]
+;; mutations
+(defmutation load-page [{:pagination/keys [list-id] :as page}]
   (action [{:keys [state] :as env}]
-    (df/load-action env [:user/by-id id] Profile {:focus [:user/like]})
-    (swap! state
-      #(-> %
-         (assoc-in [:screen.liked-articles/by-user-id id]
-           {:screen          :screen.liked-articles/by-user-id
-            :user-id         id
-            :profile-to-view [:user/by-id id]}))))
+    (df/load-action env :paginated-list/articles
+      pagination/Page {:params page
+                       :target [:screen.profile/by-user-id list-id :current-page]}))
   (remote [env]
     (df/remote-load env))
-  (refresh [env] [:profile-to-view]))
-
-(defmutation load-owned-articles-to-screen [{:user/keys [id]}]
-  (action [{:keys [state] :as env}]
-    (df/load-action env [:user/by-id id] Profile {:focus [:user/articles]}))
-  (remote [env]
-    (df/remote-load env))
-  (refresh [env] [:profile-to-view]))
+  (refresh [env]
+    [:user-id :current-page]))
