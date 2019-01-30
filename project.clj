dc69a5583d5dce7d4709365dc58628d45c9d0e75
Hoàng Minh Thắng <p@banphim.net>
2019-01-30 16:30:03 +0700

    update project deps

--- project.clj
+++ project.clj
@@ -2,18 +2,20 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/spec.alpha "0.1.143"]
-                 [duct/core "0.6.2"]
-                 [duct/module.logging "0.3.1"]
-                 [duct/module.web "0.6.4"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+
+                 [duct/core "0.7.0"]
+                 [duct/module.logging "0.4.0"]
+                 [duct/module.web "0.7.0"]
+                 [duct/module.ataraxy "0.3.0"]
+                 [duct/module.cljs "0.4.0" :exclusions [org.clojure/clojurescript]]
+                 [duct/module.sql "0.5.0"]
+
                  [duct/middleware.buddy "0.1.0"]
-                 [duct/module.ataraxy "0.2.0"]
-                 [duct/module.sql "0.4.2"]
-                 [duct/module.cljs "0.3.2" :exclusions [org.clojure/clojurescript]]
+
                  ;; this version of clojurescript doesn't cause compiling error to specter
                  ;; it's used by specter itself
-                 [org.clojure/clojurescript "1.10.126"]
+                 [org.clojure/clojurescript "1.10.439"]
                  [com.rpl/specter "1.1.1"]
                  [metosin/reitit "0.1.3"]
                  [kibu/pushy "0.3.8"]
@@ -23,7 +25,8 @@
                  [walkable "1.2.0-SNAPSHOT"]
                  [org.clojure/core.async "0.4.474"]
                  [org.postgresql/postgresql "42.1.4"]]
-  :plugins [[duct/lein-duct "0.10.6"]]
+  :middleware [lein-duct.plugin/middleware]
+  :plugins [[duct/lein-duct "0.11.2"]]
   :main ^:skip-aot conduit.main
   :resource-paths ["resources" "target/resources"]
   :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
@@ -38,12 +41,12 @@
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
                   :dependencies   [ ;; cljs
-                                   [duct/server.figwheel "0.2.1" :exclusions [org.clojure/clojurescript]]
+                                   [duct/server.figwheel "0.3.0" :exclusions [org.clojure/clojurescript]]
                                    [org.clojure/test.check "0.10.0-alpha3"]
                                    [devcards "0.2.4" :exclusions [org.clojure/clojurescript]]
 
                                    [fulcrologic/fulcro-inspect "2.1.0" :exclusions [fulcrologic/fulcro-css]]
 
-                                   [integrant/repl "0.2.0"]
-                                   [eftest "0.4.1"]
+                                   [integrant/repl "0.3.1"]
+                                   [eftest "0.5.4"]
                                    [kerodon "0.9.0"]]}})
