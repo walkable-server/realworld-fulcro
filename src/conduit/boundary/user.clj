9e95c7c6b216424fc88e9ef887cd3e1832d1d29a
Hoàng Minh Thắng <p@banphim.net>
2019-03-13 03:22:20 +0700

    remove user/by-id and user/by-username from protocol

--- src/conduit/boundary/user.clj
+++ src/conduit/boundary/user.clj
@@ -4,8 +4,6 @@
             [duct.database.sql]))
 
 (defprotocol User
-  (by-id [db id])
-  (by-username [db id])
   (create-user [db user])
   (find-login [db email password])
   (update-user [db user-id user])
@@ -31,12 +29,6 @@
     (jdbc/update! (:spec db) "\"user\""
       (-> user (select-keys [:username :name :email :bio :image :password]) hash-password)
       ["id = ?" user-id]))
-  (by-id [{db :spec} id]
-    (when-let [user (first (jdbc/find-by-keys db "\"user\"" {:id id}))]
-      (dissoc user :password)))
-  (by-username [{db :spec} username]
-    (when-let [user (first (jdbc/find-by-keys db "\"user\"" {:username username}))]
-      (dissoc user :password)))
   (find-login [{db :spec} email password]
     (when-let [user (first (jdbc/find-by-keys db "\"user\"" {:email email}))]
       (when (hashers/check password (:password user))
