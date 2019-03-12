4daa9e6d032db0cccef5b69b35328d8902e45fc0
Hoàng Minh Thắng <p@banphim.net>
2019-03-13 03:34:13 +0700

    add db helpers to dev.clj

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -4,6 +4,7 @@
             [fipp.edn :refer [pprint]]
             [clojure.tools.namespace.repl :refer [refresh]]
             [clojure.java.io :as io]
+            [clojure.java.jdbc :as jdbc]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl]
             [duct.repl.figwheel :refer [cljs-repl]]
@@ -40,3 +41,12 @@
   (load "local"))
 
 (integrant.repl/set-prep! #(duct/prep-config (read-config) profiles))
+
+(defn db []
+  (-> system (ig/find-derived-1 :duct.database/sql) val :spec))
+
+(defn q [sql]
+  (jdbc/query (db) sql))
+
+(defn e [sql]
+  (jdbc/execute! (db) sql))
