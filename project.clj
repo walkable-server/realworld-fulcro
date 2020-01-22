6e3de71e5770aebaa15b6c66d60c99e6ff2feb18
Hoàng Minh Thắng <p@banphim.net>
2020-01-22 15:29:19 +0700

    update deps

--- project.clj
+++ project.clj
@@ -2,30 +2,30 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.8.2"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
 
-                 [duct/core "0.7.0"]
-                 [duct/module.logging "0.4.0"]
+                 [duct/core "0.8.0"]
+                 [duct/module.logging "0.5.0"]
                  [duct/module.web "0.7.0"]
                  [duct/module.ataraxy "0.3.0"]
-                 [duct/module.cljs "0.4.0" :exclusions [org.clojure/clojurescript]]
-                 [duct/module.sql "0.5.0"]
+                 [duct/module.cljs "0.4.1" :exclusions [org.clojure/clojurescript]]
+                 [duct/module.sql "0.6.0"]
 
                  [duct/middleware.buddy "0.1.0"]
 
                  ;; this version of clojurescript doesn't cause compiling error to specter
                  ;; it's used by specter itself
-                 [org.clojure/clojurescript "1.10.520"]
-                 [com.rpl/specter "1.1.2"]
-                 [metosin/reitit "0.2.13"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [com.rpl/specter "1.1.3"]
+                 [metosin/reitit "0.4.2"]
                  [kibu/pushy "0.3.8"]
-                 [cheshire "5.8.1"]
+                 [cheshire "5.9.0"]
                  [duct/handler.sql "0.4.0"]
-                 [buddy/buddy-hashers "1.3.0"]
-                 [fulcrologic/fulcro "2.8.4"]
+                 [buddy/buddy-hashers "1.4.0"]
+                 [fulcrologic/fulcro "2.8.13"]
                  [walkable "1.2.0-SNAPSHOT"]
-                 [org.clojure/core.async "0.4.490"]
-                 [org.postgresql/postgresql "42.2.5"]]
+                 [org.clojure/core.async "0.7.559"]
+                 [org.postgresql/postgresql "42.2.9"]]
   :middleware [lein-duct.plugin/middleware]
   :plugins [[duct/lein-duct "0.11.2"]]
   :main ^:skip-aot conduit.main
@@ -42,13 +42,13 @@
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
                   :dependencies   [ ;; cljs
-                                   [cider/piggieback "0.4.0"]
+                                   [cider/piggieback "0.4.2"]
                                    [walkable/duct.server.figwheel "0.4.0-SNAPSHOT" :exclusions [org.clojure/clojurescript]]
-                                   [org.clojure/test.check "0.10.0-alpha3"]
+                                   [org.clojure/test.check "0.10.0"]
                                    [devcards "0.2.6" :exclusions [org.clojure/clojurescript]]
 
                                    [fulcrologic/fulcro-inspect "2.2.5" :exclusions [fulcrologic/fulcro-css]]
 
                                    [integrant/repl "0.3.1"]
-                                   [eftest "0.5.7"]
-                                   [kerodon "0.9.0"]]}})
+                                   [eftest "0.5.9"]
+                                   [kerodon "0.9.1"]]}})
