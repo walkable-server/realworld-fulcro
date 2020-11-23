a5297d9637c20d41bd1f5decfe5a72819863e26f
Hoàng Minh Thắng <p@banphim.net>
2020-11-24 06:40:46 +0700

    get rid of return/error in user

--- src/conduit/boundary/user.clj
+++ src/conduit/boundary/user.clj
@@ -1,8 +1,6 @@
 (ns conduit.boundary.user
   (:require [buddy.hashers :as hashers]
             [clojure.java.jdbc :as jdbc]
-            [conduit.ui.errors :as errors]
-            [conduit.ui.return :as return]
             [duct.database.sql]))
 
 (defprotocol User
@@ -22,8 +20,8 @@
 (defn user-error-message [msg]
   (condp re-find msg
     #"ERROR: duplicate key value violates unique constraint \"user_email_key\""
-    {:user/email [errors/email-taken]}
-    {errors/other-category [errors/unknown]}))
+    :error/email-taken
+    :error/unknown))
 
 (extend-protocol User
   duct.database.sql.Boundary
@@ -31,13 +29,13 @@
     (let [pw-hash (hashers/derive password)]
       (try
         (-> (jdbc/insert! db "\"user\""
-              (-> user (select-keys [:name :email :bio :image])
-                (assoc :password pw-hash)))
-          first (dissoc :password)
-          return/result)
+                          (-> user (select-keys [:name :email :bio :image])
+                              (assoc :password pw-hash)))
+            first
+            (dissoc :password))
         (catch org.postgresql.util.PSQLException e
           (let [msg (.getMessage e)]
-            (return/errors (user-error-message msg)))))))
+            {:error (user-error-message msg)})))))
   (update-user [db user-id user]
     (jdbc/update! (:spec db) "\"user\""
       (-> user (select-keys [:name :email :bio :image :password]) hash-password)
@@ -45,8 +43,8 @@
   (find-login [{db :spec} email password]
     (let [user (first (jdbc/find-by-keys db "\"user\"" {:email email}))]
       (if (and user (hashers/check password (:password user)))
-        (return/result (dissoc user :password))
-        (return/errors {errors/other-category [errors/can-not-log-in]}))))
+        {:user/id (:id user)}
+        {:error :error/user-not-found})))
   (follow [db follower-id followee-id]
     (jdbc/execute! (:spec db)
       [(str "INSERT INTO \"follow\" (follower_id, followee_id)"
