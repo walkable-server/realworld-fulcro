5943c837951bf8814a94e6e0b516d88076d8e79b
Hoàng Minh Thắng <p@banphim.net>
2020-12-25 15:40:25 +0700

    use session in article-preview

--- src/conduit/ui/article_preview.cljs
+++ src/conduit/ui/article_preview.cljs
@@ -1,22 +1,22 @@
 (ns conduit.ui.article-preview
   (:require
-   [fulcro.client.primitives :as prim :refer [defsc]]
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [conduit.handler.mutations :as mutations]
    [conduit.ui.other :as other]
-   [fulcro.client.mutations :as m :refer [defmutation]]
-   [fulcro.client.dom :as dom]
-   [conduit.ui.routes :as routes]))
+   [conduit.session :as session]
+   [com.fulcrologic.fulcro.dom :as dom]))
 
-(defsc ArticlePreviewMeta [this {:article/keys [author created-at liked-by-count liked-by-me]}
-                           {:keys [like unlike]}]
+(defsc ArticlePreviewMeta
+  [this {:article/keys [author created-at liked-by-count liked-by-me]}
+   {:keys [like unlike]}]
   {:query [:article/id :article/created-at :article/liked-by-count :article/liked-by-me
-           {:article/author (prim/get-query other/UserPreview)}]
-   :ident [:article/by-id :article/id]}
+           {:article/author (comp/get-query other/UserPreview)}]
+   :ident :article/id}
   (dom/div :.article-meta
-    (dom/a {:href (routes/profile-url author)}
+    (dom/a {:href (str "/profile/" (:user/id author))}
       (dom/img {:src (:user/image author other/default-user-image)}))
     (dom/div :.info
-      (dom/a :.author {:href (routes/profile-url author)}
+      (dom/a :.author {:href (str "/profile/" (:user/id author))}
         (:user/name author))
       (dom/span :.date
         (other/js-date->string created-at)))
@@ -28,43 +28,45 @@
          :onClick #(like)})
       (dom/i :.ion-heart) " " liked-by-count)))
 
-(def ui-article-preview-meta (prim/factory ArticlePreviewMeta {:keyfn :article/id}))
+(def ui-article-preview-meta (comp/factory ArticlePreviewMeta {:keyfn :article/id}))
 
-(defsc ArticlePreview [this {:article/keys [id author-id slug title description] :keys [ph/article]}
-                       {:keys [on-delete]}]
-  {:ident [:article/by-id :article/id]
+(defsc ArticlePreview
+  [this {:article/keys [id author-id slug title description]
+         :keys [ph/article] :ui/keys [current-user]}
+   {:keys [on-delete]}]
+  {:ident :article/id
+   :initial-state (fn [_] {:ui/current-user (comp/get-initial-state session/CurrentUser)
+                           :article/id :none})
    :query [:article/id :article/author-id :article/slug :article/title :article/description :article/body
-           {:ph/article (prim/get-query ArticlePreviewMeta)}]}
-  (let [whoami                     (prim/shared this :user/whoami)
-        {current-user-id :user/id} whoami]
+           {:ui/current-user (comp/get-query session/CurrentUser)}
+           {:ph/article (comp/get-query ArticlePreviewMeta)}]}
+  (let [current-user-id (:user/id current-user)]
     (dom/div :.article-preview
       (let [like #(if (number? current-user-id)
-                    (prim/transact! this `[(mutations/like {:article/id ~id})])
+                    (comp/transact! this [(mutations/like {:article/id id})])
                     (js/alert "You must log in first"))
-            unlike #(prim/transact! this `[(mutations/unlike {:article/id ~id})])]
-        (ui-article-preview-meta (prim/computed article {:like like :unlike unlike})))
+            unlike #(comp/transact! this [(mutations/unlike {:article/id id})])]
+        (ui-article-preview-meta (comp/computed article {:like like :unlike unlike})))
       (when (= current-user-id author-id)
         (dom/span :.pull-xs-right
-          (dom/a {:href (routes/to-path {:handler :screen/editor
-                                         :route-params {:article-id id}})}
+          (dom/a {:href (str "/edit/" id)}
             (dom/i :.ion-edit " "))
           (dom/i :.ion-trash-a
             {:onClick #(on-delete {:article/id id})} " ")))
       (dom/a :.preview-link
-        {:href (routes/to-path {:handler :screen/article
-                                :route-params {:article-id id :slug slug}})}
+        {:href (str "/article/" id)}
         (dom/h1 title)
         (dom/p description)
         (dom/span "Read more...")))))
 
-(def ui-article-preview (prim/factory ArticlePreview {:keyfn :article/id}))
+(def ui-article-preview (comp/factory ArticlePreview {:keyfn :article/id}))
 
 (defn article-list
-  [component articles msg-when-empty]
-  (let [delete-article (fn [{:article/keys [id] :as article}]
-                         (prim/transact! component `[(mutations/delete-article ~article)]))]
+  [component {:ui/keys [articles empty-message]} ]
+  (let [delete-article (fn [article]
+                         (comp/transact! component [(mutations/delete-article article)]))]
     (dom/div
       (if (seq articles)
-        (map (fn [a] (ui-article-preview (prim/computed a {:on-delete delete-article})))
+        (map (fn [a] (ui-article-preview (comp/computed a {:on-delete delete-article})))
           articles)
-        msg-when-empty))))
+        empty-message))))
