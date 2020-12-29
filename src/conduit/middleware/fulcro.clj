9b62aeb2ddeda306e6a729151a7f9d6a3826f58e
Hoàng Minh Thắng <p@banphim.net>
2020-12-29 11:24:32 +0700

    use default transit handler

--- src/conduit/middleware/fulcro.clj
+++ src/conduit/middleware/fulcro.clj
@@ -1,6 +1,12 @@
 (ns conduit.middleware.fulcro
+  (:import [com.cognitect.transit WriteHandler])
   (:require [com.fulcrologic.fulcro.server.api-middleware :as server]
             [integrant.core :as ig]))
+
+(deftype DefaultHandler []
+  WriteHandler
+  (tag [this v] "unknown")
+  (rep [this v] (pr-str v)))
 
 (defmethod ig/init-key ::wrap-transit
   [_ {:keys [session-auth]}]
@@ -8,4 +14,4 @@
     (-> handler
       (server/wrap-transit-params)
       session-auth
-      (server/wrap-transit-response))))
+      (server/wrap-transit-response {:opts {:default-handler (DefaultHandler.)}}))))
